904bf76569f3e2e6dbc3aed742e71d272d6aed78
johaneris <jsavalos@uamv.edu.ni>
2024-10-14 07:21:27 -0600

    Add project files.

---
+++ Sesion17/Modelos/Producto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sesion17.Modelos
+{
+    public struct Producto
+    {
+
+        //campos
+        public int ID {  get; set; }
+        public string Name {  get; set; }
+        public string Description { get; set; }
+
+        public double Precio { get; set; }
+    }
+}
